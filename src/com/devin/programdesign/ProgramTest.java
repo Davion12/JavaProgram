0a8fc0adb0d0616917eb3d01c9635b097436d50e
Devin <1206675950@qq.com>
2017-07-27 22:33:42 +0800

    Add project sources firstly

---
+++ src/com/devin/programdesign/ProgramTest.java
@@ -0,0 +1,18 @@
+package com.devin.programdesign;
+
+import java.util.Random;
+
+public class ProgramTest {
+	public static void main(String[] args) {
+	//一維數組
+	/*	int[] m = new int[10];
+		Random rnd = new Random();
+		for (int i = 0; i < m.length; i++) {
+			m[i] = rnd.nextInt(10);
+		}
+		System.out.println(m[9]);*/
+		
+	//進制轉換
+		
+	}
+}
