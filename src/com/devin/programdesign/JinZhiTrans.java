a1729963e4a5f07cd7f9812c02e3dcdc5552801a
Devin <1206675950@qq.com>
2017-07-28 01:04:19 +0800

    参考网上的代码设计

--- src/com/devin/programdesign/JinZhiTrans.java
+++ src/com/devin/programdesign/JinZhiTrans.java
@@ -1,61 +1,65 @@
 package com.devin.programdesign;
 
+import java.util.Stack;
+
 public class JinZhiTrans {
-	public static enum JinZhiEnum {
-		OCT, SHI, HEX
-	}
-
-
+	private static final String CODEING_STRING= "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$.`";
+	
 	/**
-	 * jinZhiTransfer: 进制相互转换方法，思路是通过罗列法列出，有很多情况考虑不全
-	 * @param obj : 需要转换的数据
-	 * @param frmType ： 需要转换的进制
-	 * @param toType ： 转换成的进制
+	 * baseConvert:现将非10进制需要的转换成10进制，再又10进制转换成任一进制
+	 * @param s ：输入值，以字符串输入
+	 * @param srcBase ：输入字符进制
+	 * @param destBase ：需要转换的目标进制
 	 * @return
 	 */
-	public Object jinZhiTransfer(Object obj, JinZhiEnum frmType, JinZhiEnum toType) {
-		try {
-			if ((JinZhiEnum.OCT == frmType) && (JinZhiEnum.SHI == toType)) {
-				String oct = obj.toString();
-				int octResult = Integer.parseInt(oct);
-				octResult = ((octResult / 10) * 8) + (octResult % 10);
-//				System.out.println(octResult);
-			}
-
-			else if ((JinZhiEnum.OCT == frmType) && (JinZhiEnum.HEX == toType)) {
-
-			}
-
-			else if ((JinZhiEnum.SHI == frmType) && (JinZhiEnum.HEX == toType)) {
-
-			}
-
-			else if ((JinZhiEnum.SHI == frmType) && (JinZhiEnum.OCT == toType)) {
-
-			}
-
-			else if ((JinZhiEnum.HEX == frmType) && (JinZhiEnum.OCT == toType)) {
-
-			}
-
-			else if ((JinZhiEnum.HEX == frmType) && (JinZhiEnum.SHI == toType)) {
-
-			}
-
-			else {
-				System.out.println("input frm or to type is error");
-				return obj;
-			}
-		} catch (Exception err) {
-			System.out.println(err.getMessage());
+	public String baseConvert(String s, int srcBase, int destBase)
+	{
+		if(srcBase == destBase)
+		{
+			return s;
 		}
-
-		return obj;
+		
+		char[] sr = s.toCharArray();
+		int length = sr.length;
+		if(destBase != 10)
+		{
+			s = baseConvert(s, srcBase, 10);
+		}else {
+			long n = 0;
+			for(int i = length -1; i >=0; i-- )
+			{
+				n += CODEING_STRING.indexOf(sr[i]) * Math.pow(srcBase, length -i -1);
+			}
+			return String.valueOf(n);
+			
+		}
+		
+		return shiJinZhiToAny(Integer.valueOf(s), destBase);
 	}
-	//test	
+	
+	public String shiJinZhiToAny(int intVal, int destBase)
+	{
+		StringBuilder strBuild = new StringBuilder();
+		Stack<String> sta = new Stack<String>();
+		while(intVal != 0)
+		{
+			sta.push(CODEING_STRING.charAt((int)intVal%destBase)+ "");
+			intVal/=destBase;
+		}
+		
+		while(!sta.isEmpty())
+		{
+			strBuild.append(sta.pop());
+			
+		}
+		
+		return strBuild.length() == 0 ? "0": strBuild.toString();
+	}
+	
+	//test测试
 	public static void main(String[] args) {
-		JinZhiTrans jinzhitrans = new JinZhiTrans();
-		jinzhitrans.jinZhiTransfer(75, JinZhiEnum.OCT, JinZhiEnum.SHI);
+		JinZhiTrans jinZhiTrans = new JinZhiTrans();
+		String result = jinZhiTrans.baseConvert("96", 10, 8);
+		System.out.println(result);
 	}
-
 }
