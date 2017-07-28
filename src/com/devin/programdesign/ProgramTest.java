4b6cf3c29d842b7da1d280d3e3b2ac3994b240ec
Devin <1206675950@qq.com>
2017-07-28 11:44:08 +0800

    添加输入格式错误还可继续输入的代码

--- src/com/devin/programdesign/ProgramTest.java
+++ src/com/devin/programdesign/ProgramTest.java
@@ -12,17 +12,29 @@ public class ProgramTest {
 		 * int[] m = new int[10]; Random rnd = new Random(); for (int i = 0; i <
 		 * m.length; i++) { m[i] = rnd.nextInt(10); } System.out.println(m[9]);
 		 */
-		//四则运算小测试
+		// 四则运算小测试
+		float a = 0;
+		float b = 0;
+		float result = 0;
+	
 		SiZeOperation sizeOp = new SiZeOperation();
 		System.out.println("请输入你要做的运算:\n1、加法\n2、减法\n3、乘法\n4、除法");
 		Scanner input = new Scanner(System.in);
 		int op = input.nextInt();
 		System.out.println("请输入你的计算的数值，按回车进行数值切换和结束");
-		float a = input.nextFloat();
-		float b = input.nextFloat();
-		float result = 0;
-		switch(op)
-		{
+		do {
+			try {
+				String aString = input.next();
+				String bString = input.next();
+				a = Float.parseFloat(aString);
+				b = Float.parseFloat(bString);
+				break;
+			} catch (NumberFormatException error) {
+				System.out.println("输入浮点格式错误，请重新输入");
+			}
+		} while (true);
+		
+		switch (op) {
 		case 1:
 			result = sizeOp.add(a, b);
 			break;
@@ -40,7 +52,7 @@ public class ProgramTest {
 			break;
 		}
 		
-		
-		
+		System.out.println("计算结果： " + result);
+
 	}
 }
